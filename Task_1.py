14e8c868eeb71d5e9bd46a86dc4b3e3538420b45
Denis <damned_wolf@mail.ru>
2023-05-21 10:17:56 +0300

    Task 1

---
+++ Task_1.py
@@ -0,0 +1,5 @@
+list = [len([i for i in el if i.lower() in "уеёэоаыяию"]) for el in input("Введите кричалку: ").split()]
+if all([i == list[0] for i in list]):
+    print("Парам пам-пам")
+else:
+    print("Пам парам")
